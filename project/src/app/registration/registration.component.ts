9b062a928301f556e85da396d23ebdc992f1f671
bunny <satyabanni11851@gmail.com>
2024-12-21 15:04:53 +0530

    `Update Student model and StudentService to add new field and fix API endpoints`

--- project/src/app/registration/registration.component.ts
+++ project/src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StudentService } from '../services/student.service';
 
 @Component({
   selector: 'app-registration',
@@ -8,7 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegistrationComponent {
   fg!:FormGroup;
-  constructor(private fb : FormBuilder){
+  constructor(private fb : FormBuilder,private service :StudentService){
   this.fg=this.fb.group({
     username:['',[Validators.required,Validators.minLength(5),Validators.maxLength(20)]],
     email:['',[Validators.required,Validators.email,Validators.pattern('')]],
@@ -24,4 +25,34 @@ export class RegistrationComponent {
       alert('Something went wrong');
     }
   }
+  adduser(): void {
+    if (this.fg.valid) {
+      const formData = {
+        id: undefined, // Optional field
+      src: 'default-src', // Default value; replace with actual data if available
+      alt: 'default-alt', // Default value
+      name: this.fg.value.username, // Map to form field 'username'
+      email: this.fg.value.email,   // Map to form field 'email'
+      password: this.fg.value.password, // Map to form field 'password'
+      description: 'default-description', // Default value
+      description1: 'default-description1', // Default value
+      username: this.fg.value.username, // Map to form field 'username'
+      };
+  
+      this.service.addUser(formData).subscribe({
+        next: (response) => {
+          alert('User added successfully!');
+          console.log('Response:', response); // Debugging response
+          this.fg.reset(); // Reset form after successful submission
+        },
+        error: (err) => {
+          console.error('Error while adding user:', err);
+          alert('Failed to add user. Please try again.');
+        },
+      });
+    } else {
+      alert('Please fill in all required fields correctly.');
+    }
+  }
+  
 }
